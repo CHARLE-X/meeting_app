a80e66b025185030df38df6f6852542480afc81a
CHARLE-X <81356580+CHARLE-X@users.noreply.github.com>
2024-07-15 09:25:37 +0100

    Update MeetingDetailsPage.tsx

--- app/dashboard/viewmeetings/MeetingDetailsPage.tsx
+++ app/dashboard/viewmeetings/MeetingDetailsPage.tsx
@@ -1,6 +1,19 @@
+// import React from 'react';
+// import SuspenseBoundaryWrapper from '@/app/dashboard/component/SuspenseBoundaryWrapper';
+// import MeetingDetails from '@/app/dashboard/viewmeetings/page';
+
+// const MeetingDetailsPage: React.FC = () => {
+//   return (
+//     <SuspenseBoundaryWrapper>
+//       <MeetingDetails />
+//     </SuspenseBoundaryWrapper>
+//   );
+// };
+
+// export default MeetingDetailsPage;
 import React from 'react';
 import SuspenseBoundaryWrapper from '@/app/dashboard/component/SuspenseBoundaryWrapper';
-import MeetingDetails from '@/app/dashboard/viewmeetings/page';
+import MeetingDetails from '@/app/dashboard/viewmeetings/MeetingDetails';
 
 const MeetingDetailsPage: React.FC = () => {
   return (
